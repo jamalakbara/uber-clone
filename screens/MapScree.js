117de896ca76ead06128b6cb3f68314d4cffd2b5
akbar <jamalakbaralam@gmail.com>
2023-02-19 14:53:39 +0700

    first commit

---
+++ screens/MapScree.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MapScree = () => {
+  return (
+    <View>
+      <Text>Here is the map stuff</Text>
+    </View>
+  )
+}
+
+export default MapScree
+
+const styles = StyleSheet.create({})
